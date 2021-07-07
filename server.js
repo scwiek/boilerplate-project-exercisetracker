8a019e9b64597aa63ff6426d103d371afe1693da
Sean Cwiek <16272933+scwiek@users.noreply.github.com>
2021-07-06 22:54:17 -0400

    Just starting.

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const {
+  v4: uuidv4
+} = require('uuid');
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({
+  extended: true
+}));
+
+var users = [];
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res) => {
+  let username = req.body.username;
+  let uuid = uuidv4();
+  let user = {
+    user: username,
+    _id: uuid
+  };
+  users.push(user);
+  res.json(user);
+});
 
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
 
+app.post('/api/users/:_id/exercises', (req, res) => {
+  // TODO: More date formatting/validation
+  let uid = req.params._id;
+  let desc = req.body.description;
+  let dur = req.body.duration;
+  let fullDate = req.body.date;
+  if (!fullDate) {
+    let todayDate = new Date();
+    let year = todayDate.getFullYear();
+    let month = ((todayDate.getMonth() + 1).toString().length) === 1 ? '0' + (todayDate.getMonth() + 1) : (todayDate.getMonth() + 1);
+    let monthDate = (todayDate.getDate().toString().length) === 1 ? '0' + todayDate.getDate() : todayDate.getDate();
+    fullDate = `${year}-${month}-${monthDate}`;
+  }
+  res.json({
+    _id: uid,
+    description: desc,
+    duration: dur,
+    date: fullDate
+  });
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
