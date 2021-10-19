01fb0729ad4e09071a72668779dd69c21d1f3477
Sean Cwiek <16272933+scwiek@users.noreply.github.com>
2021-10-18 23:15:55 -0400

    WIP

--- server.js
+++ server.js
@@ -22,40 +22,68 @@ app.post('/api/users', (req, res) => {
   let username = req.body.username;
   let uuid = uuidv4();
   let user = {
-    user: username,
-    _id: uuid
+    username: username,
+    _id: uuid,
+    log: []
   };
   users.push(user);
-  res.json(user);
+  res.json((({username, _id}) => ({username, _id}))(user));
 });
 
 app.get('/api/users', (req, res) => {
-  res.json(users);
+  console.log(users);
+  let userList = [];
+  users.forEach(x => userList.push({user: x.user, _id: x._id}));
+  res.json(userList);
 });
 
 app.post('/api/users/:_id/exercises', (req, res) => {
-  // TODO: More date formatting/validation
   let uid = req.params._id;
+  let selectedUser = users.find( ({_id}) => _id === uid);
+  let selectedUserName = selectedUser.username;
+  console.log(selectedUserName);
+  console.log(selectedUser);
   let desc = req.body.description;
   let dur = req.body.duration;
-  let fullDate = req.body.date;
+  let inputDate = req.body.date;
+  let fullDate = new Date(inputDate).toDateString();
   if (!fullDate) {
     let todayDate = new Date();
-    let year = todayDate.getFullYear();
-    let month = ((todayDate.getMonth() + 1).toString().length) === 1 ? '0' + (todayDate.getMonth() + 1) : (todayDate.getMonth() + 1);
-    let monthDate = (todayDate.getDate().toString().length) === 1 ? '0' + todayDate.getDate() : todayDate.getDate();
-    fullDate = `${year}-${month}-${monthDate}`;
+    fullDate = todayDate.toDateString();
   }
-  res.json({
-    _id: uid,
+  let exercise = {
     description: desc,
-    duration: dur,
-    date: fullDate
-  });
+    duration: Number(dur),
+    date: Date(fullDate)
+  };
+  selectedUser.log.push(exercise);
+  console.log(selectedUser);
+  // res.json({
+  //   username: selectedUserName, 
+  //   _id: uid,
+  //   description: desc,
+  //   duration: dur,
+  //   date: fullDate
+  // });
+  res.json(selectedUser);
 });
 
 app.get('/api/users/:_id/logs', (req, res) => {
-
+  let fromDate = req.query.from;
+  let toDate = req.query.to;
+  let resLimit = req.query.limit;
+  console.log(resLimit);
+  let uid = req.params._id;
+  let selectedUser = users.find( ({_id}) => _id === uid);
+  selectedUser.count = selectedUser.log.length;
+  // TODO: Add options from, to
+  if (resLimit) {
+    retUser = {username: selectedUser.username, _id: uid, log: selectedUser.log.slice(0,resLimit)};
+    res.json(retUser);
+  } else {
+    console.log(selectedUser);
+    res.json(selectedUser);
+  }
 });
 
 
